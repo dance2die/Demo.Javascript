c272ef221a0bd1a40e0915b40cd2dcc62a5895ce
dance2die <dontbothermewithspam@gmail.com>
2015-05-03 21:10:01 -0400

    Hogan test

---
+++ Demo.Javascript/Demo.Javascript.Template.Hogan/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Javascript.Template.Hogan.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
