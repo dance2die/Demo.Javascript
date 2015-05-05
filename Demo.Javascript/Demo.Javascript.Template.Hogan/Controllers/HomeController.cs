5e54f6490267d60b3aa09f209bd208f9b838ce12
dance2die <dontbothermewithspam@gmail.com>
2015-05-04 21:05:59 -0400

    Can't yet figure out why jQuery.html() returns wrong HTML... http://stackoverflow.com/questions/30042500/jquery-returns-wrong-html

--- Demo.Javascript/Demo.Javascript.Template.Hogan/Controllers/HomeController.cs
+++ Demo.Javascript/Demo.Javascript.Template.Hogan/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using Demo.Javascript.Template.Hogan.Models;
 
 namespace Demo.Javascript.Template.Hogan.Controllers
 {
@@ -11,6 +13,32 @@ namespace Demo.Javascript.Template.Hogan.Controllers
 		public ActionResult Index()
 		{
 			return View();
+		}
+
+		public ActionResult People()
+		{
+			List<Person> people = GetPeople();
+			var peopleList = people.Select(person => new
+			{
+				FirstName = person.FirstName,
+				LastName = person.LastName,
+				FullName = string.Format("{0}, {1}", person.LastName, person.FirstName)
+			});
+			//string json = new JavaScriptSerializer().Serialize(peopleList);
+			//return View((object)json);
+			return View(people);
+		}
+
+		private List<Person> GetPeople()
+		{
+			return new List<Person>
+				{
+					new Person("John", "Doe"),
+					new Person("Jane", "Doe"),
+					new Person("Robert", "Smith"),
+					new Person("Mike", "Durden"),
+					new Person("Marilin", "Monroe"),
+				};
 		}
 	}
 }
---
+++ Demo.Javascript/Demo.Javascript.Template.Hogan/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Demo.Javascript.Template.Hogan.Models
+{
+	public class Person
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+
+		public Person()
+		{
+		}
+
+		public Person(string firstName, string lastName)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+		}
+	}
+}
