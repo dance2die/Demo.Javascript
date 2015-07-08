b1d0c95bb87f78d7ab12fdcc0a5874b0112af471
dance2die <dontbothermewithspam@gmail.com>
2015-07-07 20:21:03 -0400

    Couldn't fix Angular UI Grid's up and down icons

---
+++ Demo.Javascript/Demo.Javascript.AngularJs.AngularUIGrid/Scripts/Home.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.edit']);
+
+app.controller('MainCtrl', ['$scope', function ($scope) {
+
+	$scope.gridOptions = {
+		enableSorting: true,
+		columnDefs: [
+          { name: 'firstName', field: 'first-name' },
+          { name: '1stFriend', field: 'friends[0]' },
+          { name: 'city', field: 'address.city' },
+          { name: 'getZip', field: 'getZip()', enableCellEdit: false }
+		],
+		data: [{
+			"first-name": "Cox",
+			"friends": ["friend0"],
+			"address": { street: "301 Dove Ave", city: "Laurel", zip: "39565" },
+			"getZip": function () { return this.address.zip; }
+		}
+		]
+	};
+
+}]);
